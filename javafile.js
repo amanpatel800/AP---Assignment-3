6f47b812e2f9727dd5904c8bfb063ae233e7f251
amanpatel800 <ap7996@nyu.edu>
2024-04-10 22:11:34 -0400

    testing the popups

--- javafile.js
+++ javafile.js
@@ -1,14 +1,31 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYW1hbnBhdGVsODAwIiwiYSI6ImNsdXVma3Y2eDA5aTYyaXJ1cG9paWxkeXkifQ.W3N3M_9eewZA6lABLZL3Pw';
-        const startermap = new mapboxgl.Map({
-            container: 'startermap',
-            style: 'mapbox://styles/mapbox/navigation-night-v1',
-            center: [-73.935242, 40.730610],
-            zoom: 11
-        });
+// mapboxgl.accessToken = 'pk.eyJ1IjoiYW1hbnBhdGVsODAwIiwiYSI6ImNsdXVma3Y2eDA5aTYyaXJ1cG9paWxkeXkifQ.W3N3M_9eewZA6lABLZL3Pw';
+//         const startermap = new mapboxgl.Map({
+//             container: 'startermap',
+//             style: 'mapbox://styles/mapbox/navigation-night-v1',
+//             center: [-73.935242, 40.730610],
+//             zoom: 11
+//         });
 
-document.addEventListener('DOMContentLoaded', function() {
-    var pennButton = document.getElementById('pennButton');
-    pennButton.addEventListener('click', function() {
-        startermap.flyTo({center: [-70, 41], zoom: 9});;
-    });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     var pennButton = document.getElementById('pennButton');
+//     pennButton.addEventListener('click', function() {
+//         startermap.flyTo({center: [-70, 41], zoom: 9});;
+//     });
+// });
+
+// Get the popup container and its content
+var popupContainer = document.getElementById("popupContainer");
+var popupContent = document.getElementById("popupContent");
+
+// Get the Penn Station button
+var pennButton = document.getElementById("pennButton");
+
+// When the user clicks the Penn Station button, toggle the visibility of the popup
+pennButton.onclick = function() {
+  if (popupContainer.style.display === "block") {
+    popupContainer.style.display = "none";
+  } else {
+    popupContainer.style.display = "block";
+  }
+}
+
